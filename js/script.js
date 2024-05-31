4d1e15b54efedf20e48c3c6edbf28e6b7ff39067
MatthewPCope <matthew.p.cope@gmail.com>
2024-05-31 11:02:48 -0700

    sticky nav

--- js/script.js
+++ js/script.js
@@ -1,8 +1,64 @@
+// set current year
 const yearEl = document.querySelector(".year")
 const currentYear = new Date().getFullYear()
 yearEl.textContent = currentYear
 
+// make mobile nav work
+const btnNavEl = document.querySelector('.btn-mobile-nav')
+const headerEl = document.querySelector(".header")
 
+btnNavEl.addEventListener('click', () => {
+  headerEl.classList.toggle("nav-open")
+})
+
+// Sticky Navigation //
+
+const sectionHeroEl = document.querySelector('.section-hero')
+
+const observer = new IntersectionObserver(function(entries){
+  const ent = entries[0]
+  if(ent.isIntersecting === false){
+    document.body.classList.add('sticky')
+  }
+  if (ent.isIntersecting === true) {
+    document.body.classList.remove("sticky");
+  }
+}, 
+{
+  root: null,
+  threshold: 0,
+  rootMargin: '-80px'
+})
+observer.observe(sectionHeroEl)
+
+///////////////////////////////////////////////////////////
+// Smooth scrolling animation ***this doesn't need to be here, it works without it*****
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile naviagtion
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
